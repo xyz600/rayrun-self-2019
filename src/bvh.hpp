419c8f6042f8717f8dcaeea5808e8c5add9e2a95
yuichi.sayama <xyz600600@gmail.com>
2019-08-18 19:34:01 +0900

    temporary push(with some bugs)

--- src/bvh.hpp
+++ src/bvh.hpp
@@ -31,6 +31,31 @@ public:
 	using node_index_t = std::int32_t;
 
 private:
+
+	struct LeafNode {
+		node_index_t leaf_meshid_from;
+		node_index_t leaf_meshid_to;
+		node_index_t start_mesh_index;
+
+		LeafNode()
+			: leaf_meshid_from(0)
+			, leaf_meshid_to(0)
+			, start_mesh_index(0) {}
+
+		LeafNode(node_index_t leaf_meshid_from, node_index_t leaf_meshid_to, node_index_t start_mesh_index)
+			: leaf_meshid_from(leaf_meshid_from), leaf_meshid_to(leaf_meshid_to), start_mesh_index(start_mesh_index) {}
+
+		LeafNode(LeafNode &&src)
+			: leaf_meshid_from(src.leaf_meshid_from), leaf_meshid_to(src.leaf_meshid_to), start_mesh_index(src.start_mesh_index) {}
+
+		void operator=(LeafNode &&src)
+		{
+			leaf_meshid_from = src.leaf_meshid_from;
+			leaf_meshid_to = src.leaf_meshid_to;
+			start_mesh_index = src.start_mesh_index;
+		}
+	};
+
 	struct Node {
 	public:
 		node_index_t self;
@@ -40,30 +65,33 @@ private:
 		// AABB
 		AABB aabb;
 
-		meshid_index_t leaf_meshid_from;
-		meshid_index_t leaf_meshid_to;
-		meshid_index_t start_mesh_index;
-
 		Node() noexcept { clear(); }
 
 		Node(std::int32_t self) noexcept : self(self) { clear(); }
 
 		void clear() noexcept {
 			left = right = SimpleBVH::Invalid;
-			leaf_meshid_from = SimpleBVH::Invalid;
-			leaf_meshid_to = SimpleBVH::Invalid;
 			aabb.clear();
 		}
 
 		void set_leaf(const std::size_t leaf_from,
-			const std::size_t leaf_to) noexcept {
-			this->leaf_meshid_from = leaf_from;
-			this->leaf_meshid_to = leaf_to;
-			left = right = SimpleBVH::Invalid;
+			const std::size_t leaf_to, LeafNode &leaf_node) noexcept {
+			leaf_node.leaf_meshid_from = leaf_from;
+			leaf_node.leaf_meshid_to = leaf_to;
 		}
 
+		node_index_t leaf_index() const noexcept {
+			assert(is_leaf());
+			return -left;
+		}
+
+		void set_leaf_index(const node_index_t leaf_index) noexcept {
+			left = -leaf_index;
+		}
+
+
 		bool is_leaf() const noexcept {
-			return left == SimpleBVH::Invalid && right == SimpleBVH::Invalid;
+			return left < 0;
 		}
 
 		void copy_from(const Node &src) noexcept {
@@ -71,9 +99,6 @@ private:
 			left = src.left;
 			right = src.right;
 			aabb = src.aabb;
-			leaf_meshid_from = src.leaf_meshid_from;
-			leaf_meshid_to = src.leaf_meshid_to;
-			start_mesh_index = src.start_mesh_index;
 		}
 	};
 
@@ -88,9 +113,6 @@ private:
 			for (std::size_t i = 0; i < 8; i++) {
 				const std::size_t index = std::min(i, node_list.size() - 1);
 				aabb_list.push_back(&node_list[index]->aabb);
-				leaf_meshid_from[i] = node_list[index]->leaf_meshid_from;
-				leaf_meshid_to[i] = node_list[index]->leaf_meshid_to;
-				start_mesh_index[i] = node_list[index]->start_mesh_index;
 			}
 			aabb.construct(aabb_list);
 			node_size_in_children = node_list.size();
@@ -99,9 +121,6 @@ private:
 		Nodex8(Nodex8 &&src) {
 			self = src.self;
 			std::copy(src.children.begin(), src.children.end(), children.begin());
-			std::copy(src.leaf_meshid_from.begin(), src.leaf_meshid_from.end(), leaf_meshid_from.begin());
-			std::copy(src.leaf_meshid_to.begin(), src.leaf_meshid_to.end(), leaf_meshid_to.begin());
-			std::copy(src.start_mesh_index.begin(), src.start_mesh_index.end(), start_mesh_index.begin());
 			node_size_in_children = src.node_size_in_children;
 			aabb = std::forward<PackedAABBx8>(src.aabb);
 		}
@@ -109,24 +128,23 @@ private:
 		void operator=(Nodex8 &&src) {
 			self = src.self;
 			std::copy(src.children.begin(), src.children.end(), children.begin());
-			std::copy(src.leaf_meshid_from.begin(), src.leaf_meshid_from.end(), leaf_meshid_from.begin());
-			std::copy(src.leaf_meshid_to.begin(), src.leaf_meshid_to.end(), leaf_meshid_to.begin());
-			std::copy(src.start_mesh_index.begin(), src.start_mesh_index.end(), start_mesh_index.begin());
 			node_size_in_children = src.node_size_in_children;
 			aabb = std::forward<PackedAABBx8>(src.aabb);
 		}
 
 		bool is_leaf(const std::size_t index) const noexcept {
-			return children[index] == LeafID;
+			return children[index] < 0;
 		}
 
-		node_index_t self;
+		node_index_t leaf_index(const std::size_t index) const noexcept {
+			assert(is_leaf(index));
+			return -children[index];
+		};
+
 		PackedAABBx8 aabb;
 		std::array<node_index_t, 8> children;
-		std::array <meshid_index_t, 8> leaf_meshid_from;
-		std::array<meshid_index_t, 8> leaf_meshid_to;
-		std::array<meshid_index_t, 8> start_mesh_index;
-		std::size_t node_size_in_children;
+		node_index_t self;
+		node_index_t node_size_in_children;
 	};
 
 	struct Range {
@@ -186,9 +204,8 @@ private:
 
 	static constexpr std::int32_t leaf_size_threashold = 16;
 
-	void
-		validate_range(const std::array<std::vector<meshid_index_t>, 3> &mesh_id_sorted_by,
-			const Range &range) const noexcept;
+	void validate_range(const std::array<std::vector<meshid_index_t>, 3> &mesh_id_sorted_by,
+		const Range &range) const noexcept;
 
 	void validate_order(const std::vector<node_index_t> &order);
 
@@ -206,6 +223,7 @@ private:
 	// ノード
 	std::vector<Node> m_node_list;
 	std::vector<Nodex8> m_nodex8_list;
+	std::vector<LeafNode> m_leafnode_list;
 	MeshTriangleList m_mesh_list;
 	std::vector<PackedTrianglex8> m_packed_triangle_list;
 };
@@ -311,19 +329,21 @@ void SimpleBVH::setup_packed_triangle() noexcept {
 			que.push(node.right);
 		}
 		else {
-			const std::size_t mesh_index_from = packed_leaf_index;
-			node.start_mesh_index = node.leaf_meshid_from;
+			LeafNode &leaf_node = m_leafnode_list[node.leaf_index()];
 
-			for (meshid_index_t mesh_index = node.leaf_meshid_from;
-				mesh_index < node.leaf_meshid_to; mesh_index += 8) {
-				const std::size_t to_index =
-					std::min(mesh_index + 8, node.leaf_meshid_to);
+			const std::size_t mesh_index_from = packed_leaf_index;
+			leaf_node.start_mesh_index = leaf_node.leaf_meshid_from;
+
+			for (meshid_index_t mesh_index = leaf_node.leaf_meshid_from;
+				mesh_index < leaf_node.leaf_meshid_to; mesh_index += 8) {
+				const auto to_index =
+					std::min<meshid_index_t>(mesh_index + 8, leaf_node.leaf_meshid_to);
 				m_packed_triangle_list.emplace_back(m_mesh_list.begin() + mesh_index,
 					m_mesh_list.begin() + to_index);
 				packed_leaf_index++;
 			}
-			node.leaf_meshid_from = mesh_index_from;
-			node.leaf_meshid_to = packed_leaf_index;
+			leaf_node.leaf_meshid_from = mesh_index_from;
+			leaf_node.leaf_meshid_to = packed_leaf_index;
 		}
 	}
 }
@@ -332,6 +352,7 @@ bool SimpleBVH::construct(MeshTriangleList &&mesh_list) {
 	m_mesh_list = std::forward<MeshTriangleList>(mesh_list);
 	m_node_list.emplace_back(0);
 	m_node_list.resize(m_mesh_list.size() * 2);
+	m_leafnode_list.resize(m_mesh_list.size() * 2);
 
 	const std::size_t num_mesh = m_mesh_list.size();
 
@@ -362,6 +383,8 @@ bool SimpleBVH::construct(MeshTriangleList &&mesh_list) {
 
 	std::atomic_int32_t node_index = 0;
 
+	std::atomic_int32_t leaf_index = 1;
+
 	// AABB of range [0, index]
 	std::vector<AABB> left_accumulated_AABB(num_mesh);
 
@@ -387,7 +410,11 @@ bool SimpleBVH::construct(MeshTriangleList &&mesh_list) {
 					auto &current_node = m_node_list[range.node_index];
 
 					if (range.size() <= leaf_size_threashold) {
-						current_node.set_leaf(range.from, range.to);
+
+						const auto local_leaf_index = leaf_index++;
+						current_node.set_leaf_index(local_leaf_index);
+						current_node.set_leaf(range.from, range.to, m_leafnode_list[local_leaf_index]);
+
 						// node is already cleared when node is created
 						for (meshid_index_t mesh_index = range.from; mesh_index < range.to;
 							mesh_index++) {
@@ -405,7 +432,10 @@ bool SimpleBVH::construct(MeshTriangleList &&mesh_list) {
 							&best_position);
 
 						if (best_axis == Invalid) {
-							current_node.set_leaf(range.from, range.to);
+							const auto local_leaf_index = leaf_index++;
+							current_node.set_leaf_index(local_leaf_index);
+							current_node.set_leaf(range.from, range.to, m_leafnode_list[local_leaf_index]);
+
 							// node is already cleared when node is created
 							for (meshid_index_t mesh_index = range.from; mesh_index < range.to;
 								mesh_index++) {
@@ -444,6 +474,9 @@ bool SimpleBVH::construct(MeshTriangleList &&mesh_list) {
 	m_node_list.resize(++node_index);
 	m_node_list.shrink_to_fit();
 
+	m_leafnode_list.resize(leaf_index);
+	m_leafnode_list.shrink_to_fit();
+
 	reorder_node(5);
 	reorder_mesh(mesh_index_list);
 
@@ -451,7 +484,7 @@ bool SimpleBVH::construct(MeshTriangleList &&mesh_list) {
 
 	setup_packed_node();
 
-	//validate_packed_node();
+	validate_packed_node();
 
 	return true;
 }
@@ -486,8 +519,9 @@ void SimpleBVH::validate_node_index() {
 void SimpleBVH::validate_node(int node_index) {
 	auto &node = m_node_list[node_index];
 	if (node.is_leaf()) {
-		for (std::int32_t mesh_index = node.leaf_meshid_from;
-			mesh_index < node.leaf_meshid_to; mesh_index++) {
+		const LeafNode &leaf_node = m_leafnode_list[node.leaf_index()];
+		for (auto mesh_index = leaf_node.leaf_meshid_from;
+			mesh_index < leaf_node.leaf_meshid_to; mesh_index++) {
 			assert(node.aabb.contain(m_mesh_list[mesh_index]));
 		}
 	}
@@ -503,8 +537,8 @@ void SimpleBVH::validate_leaf() {
 	std::set<meshid_index_t> mesh_ids;
 	for (auto node : m_node_list) {
 		if (node.is_leaf()) {
-			for (meshid_index_t id = node.leaf_meshid_from; id < node.leaf_meshid_to;
-				id++) {
+			const auto &leaf_node = m_leafnode_list[node.leaf_index()];
+			for (meshid_index_t id = leaf_node.leaf_meshid_from; id < leaf_node.leaf_meshid_to; id++) {
 				assert(0 <= id && id < m_mesh_list.size());
 				mesh_ids.insert(id);
 			}
@@ -526,7 +560,8 @@ void SimpleBVH::collect_range_parallel(int node_index, std::vector<std::pair<int
 	const auto &node = m_nodex8_list[node_index];
 	for (std::size_t i = 0; i < node.node_size_in_children; i++) {
 		if (node.is_leaf(i)) {
-			result.emplace_back(node.leaf_meshid_from[i], node.leaf_meshid_to[i]);
+			const auto &leaf_node = m_leafnode_list[node.leaf_index(i)];
+			result.emplace_back(leaf_node.leaf_meshid_from, leaf_node.leaf_meshid_to);
 		}
 		else {
 			collect_range_parallel(node.children[i], result);
@@ -537,7 +572,8 @@ void SimpleBVH::collect_range_parallel(int node_index, std::vector<std::pair<int
 void SimpleBVH::collect_range_single(int node_index, std::vector<std::pair<int, int>> &result) {
 	const auto &node = m_node_list[node_index];
 	if (node.is_leaf()) {
-		result.emplace_back(node.leaf_meshid_from, node.leaf_meshid_to);
+		const auto &leaf_node = m_leafnode_list[node.leaf_index()];
+		result.emplace_back(leaf_node.leaf_meshid_from, leaf_node.leaf_meshid_to);
 	}
 	else {
 		collect_range_single(node.left, result);
@@ -617,8 +653,9 @@ bool SimpleBVH::intersectAnySub(node_index_t nodeIndex, RayExt &ray,
 
 		for (auto idx : valid_index) {
 			if (node.is_leaf(idx)) {
-				for (meshid_index_t mesh_index = node.leaf_meshid_from[idx];
-					mesh_index < node.leaf_meshid_to[idx]; mesh_index++) {
+				const auto &leaf_node = m_leafnode_list[node.leaf_index(idx)];
+				for (meshid_index_t mesh_index = leaf_node.leaf_meshid_from;
+					mesh_index < leaf_node.leaf_meshid_to; mesh_index++) {
 					if (m_packed_triangle_list[mesh_index].intersect_distance(ray) !=
 						PackedTrianglex8::InvalidIndex) {
 						return true;
@@ -756,14 +793,16 @@ void SimpleBVH::reorder_mesh(
 	std::int32_t new_mesh_index = 0;
 	for (auto &node : m_node_list) {
 		if (node.is_leaf()) {
-			const std::int32_t new_meshid_from = new_mesh_index;
-			for (std::int32_t old_mesh_index = node.leaf_meshid_from;
-				old_mesh_index < node.leaf_meshid_to; old_mesh_index++) {
+
+			auto &leaf_node = m_leafnode_list[node.leaf_index()];
+			const meshid_index_t new_meshid_from = new_mesh_index;
+			for (auto old_mesh_index = leaf_node.leaf_meshid_from;
+				old_mesh_index < leaf_node.leaf_meshid_to; old_mesh_index++) {
 				order[mesh_index_list[old_mesh_index]] = new_mesh_index;
 				new_mesh_index++;
 			}
-			node.leaf_meshid_from = new_meshid_from;
-			node.leaf_meshid_to = new_mesh_index;
+			leaf_node.leaf_meshid_from = new_meshid_from;
+			leaf_node.leaf_meshid_to = new_mesh_index;
 		}
 	}
 	assert(new_mesh_index == m_mesh_list.size());
@@ -845,13 +884,14 @@ bool SimpleBVH::intersectSub(std::int32_t nodeIndex, RayExt &ray,
 		for (auto idx : valid_index) {
 			if (distance_array[idx] < ray.tfar) {
 				if (node.is_leaf(idx)) {
-					for (std::int32_t mesh_index = node.leaf_meshid_from[idx];
-						mesh_index < node.leaf_meshid_to[idx]; mesh_index++) {
+					const auto &leaf_node = m_leafnode_list[node.leaf_index(idx)];
+					for (std::int32_t mesh_index = leaf_node.leaf_meshid_from;
+						mesh_index < leaf_node.leaf_meshid_to; mesh_index++) {
 						const std::size_t subindex =
 							m_packed_triangle_list[mesh_index].intersect_distance(ray);
 						if (subindex != PackedTrianglex8::InvalidIndex) {
-							*hitMeshIndex = node.start_mesh_index[idx] +
-								8 * (mesh_index - node.leaf_meshid_from[idx]) + subindex;
+							*hitMeshIndex = leaf_node.start_mesh_index +
+								8 * (mesh_index - leaf_node.leaf_meshid_from) + subindex;
 							success = true;
 						}
 					}
